ee8aaeffbbd2d6ea208fdb4c4484155eee1bd170
Tunna <tmajmundar@gmail.com>
2016-07-30 09:50:46 -0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,87 @@
+def add(num1, num2):
+	return num1 + num2
+
+print add(4,5)
+
+
+
+def subtract(num1, num2):
+	return num1 - num2
+
+print subtract(4,5)
+
+
+
+def multiply(num1, num2):
+	return num1 * num2
+
+print multiply(4,5)
+
+
+
+def divide(num1, num2):
+	return num1 / num2
+
+print divide(4,5)
+
+
+
+def modulo(num1, num2):	
+	return num1 % num2
+
+print modulo(5,2)
+
+
+
+def power(num1, num2):
+	return num1 ** num2
+
+print power(4,5)
+
+
+
+def square(num):
+	return num ** 2
+
+print square(4)
+
+
+print add(add(4,5), subtract(9,6))
+
+print subtract(divide(12,2), 60)
+
+print add(add(1,2), 3)
+
+print square(add(1,2))
+
+print divide(modulo(3,4), multiply(9,9))
+
+print multiply((7), add(3,8))
+
+print subtract((add(1,2), multiply(add(4,5),3))
+
+print power(add(2,3), 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
